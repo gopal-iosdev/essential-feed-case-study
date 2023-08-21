dbfde9bcc83cddac41963c291aa20169bdda25e2
gopal-iosdev <gopal.iosdev@gmail.com>
2023-08-20 23:33:07 -0400

    Added readme and architecture files

---
+++ EssentialFeed/EssentialFeed/FeedFeature/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Gopal Gurram on 4/15/23.
+//
+
+import Foundation
+
+public enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+public protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> (Void))
+}
