faf58e4c913a769c7ef1574ff1359bca2bb5cd68
gopal-iosdev <gopal.iosdev@gmail.com>
2024-01-28 11:33:42 -0500

    Add EssentialFeedCacheIntegrationTests target to seperate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Gopal Gurram on 1/28/24.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
