39468e51130b51a698b8673f8d4376e8d21c6a0f
gopal-iosdev <gopal.iosdev@gmail.com>
2024-02-21 08:58:37 -0500

    Add shimmering animation while loading image in the prototype app

--- Prototype/Prototype/FeedImageViewModel+PrototypeData.swift
+++ Prototype/Prototype/FeedImageViewModel+PrototypeData.swift
@@ -13,32 +13,32 @@ extension FeedImageViewModel {
             FeedImageViewModel(
                 description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
                 location: "East Side Gallery\nMemorial in Berlin, Germany",
-                imageName: "image-0"
+                imageName: "Image-0"
             ),
             FeedImageViewModel(
                 description: nil,
                 location: "Cannon Street, London",
-                imageName: "image-1"
+                imageName: "Image-1"
             ),
             FeedImageViewModel(
                 description: "The Desert Island in Faro is beautiful!! ☀️",
                 location: nil,
-                imageName: "image-2"
+                imageName: "Image-2"
             ),
             FeedImageViewModel(
                 description: nil,
                 location: nil,
-                imageName: "image-3"
+                imageName: "Image-3"
             ),
             FeedImageViewModel(
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales. At 1,500 feet in length, it is the second-longest pier in Wales, and the ninth longest in the British Isles.",
                 location: "Garth Pier\nNorth Wales",
-                imageName: "image-4"
+                imageName: "Image-4"
             ),
             FeedImageViewModel(
                 description: "Glorious day in Brighton!! 🎢",
                 location: "Brighton Seafront",
-                imageName: "image-5"
+                imageName: "Image-5"
             )
         ]
     }
