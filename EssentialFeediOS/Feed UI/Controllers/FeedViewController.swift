edbc19a74cf64ac733fc45380b5c46551cb1927c
gopal-iosdev <gopal.iosdev@gmail.com>
2024-04-22 09:02:48 -0400

    Feed loading error UI & Presentation

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -87,3 +87,9 @@ extension FeedViewController: FeedLoadingView {
         }
     }
 }
+
+extension FeedViewController: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        errorView.message = viewModel.message
+    }
+}
