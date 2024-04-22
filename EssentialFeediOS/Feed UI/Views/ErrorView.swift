562bdbc8557def15d4ef84df6da67a59bb3e5fdf
gopal-iosdev <gopal.iosdev@gmail.com>
2024-04-22 09:13:51 -0400

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Gopal Gurram on 4/22/24.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
