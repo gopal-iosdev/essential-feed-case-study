5ff5950d5f1fce7d4c907eae0521ff04f003cf89
gopal-iosdev <gopal.iosdev@gmail.com>
2024-04-23 10:23:52 -0400

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Gopal Gurram on 4/23/24.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
