c04a2d9496046297c72924e90866927afeb6685a
gopal-iosdev <gopal.iosdev@gmail.com>
2024-04-23 10:27:49 -0400

    Extract refreshing logic to an extension

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -76,11 +76,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
 extension FeedViewController: FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
 }
 
