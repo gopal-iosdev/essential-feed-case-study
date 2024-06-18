16f1dc9add3efcb1a56f5d78937260fa1cacbea3
gopal-iosdev <gopal.iosdev@gmail.com>
2024-06-18 09:41:13 -0400

    Updated order for EssentialFeed folders and add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Gopal Gurram on 6/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
