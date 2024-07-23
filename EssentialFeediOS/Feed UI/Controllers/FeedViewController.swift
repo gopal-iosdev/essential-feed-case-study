cd97064d6ea564391dec7317e66cb36bc5e75b2c
gopal-iosdev <gopal.iosdev@gmail.com>
2024-07-23 09:30:44 -0400

    Fix potential crash when cancelling requests in UITableView didEndDisplayingCell method

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -17,6 +17,8 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
     @IBOutlet private(set) public var errorView: ErrorView?
 
+    private var loadingControllers = [IndexPath: FeedImageCellController]()
+
     private var onViewIsAppearing: ((FeedViewController) -> Void)?
 
     private var tableModel = [FeedImageCellController]() {
@@ -51,6 +53,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     public func display(_ cellControllers: [FeedImageCellController]) {
+        loadingControllers = [:]
         tableModel = cellControllers
     }
 
@@ -77,11 +80,14 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
-        tableModel[indexPath.row]
+        let controller = tableModel[indexPath.row]
+        loadingControllers[indexPath] = controller
+        return controller
     }
 
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(forRowAt: indexPath).cancelLoad()
+        loadingControllers[indexPath]?.cancelLoad()
+        loadingControllers[indexPath] = nil
     }
 }
 
