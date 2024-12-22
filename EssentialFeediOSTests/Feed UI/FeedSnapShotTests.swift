a998bd926c66d920c7cc9ddcb67e387fa2c7c373
gopal-iosdev <gopal.iosdev@gmail.com>
2024-12-22 07:19:18 -0500

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedSnapShotTests.swift
@@ -18,6 +18,15 @@ final class FeedSnapShotTests: XCTestCase {
 
         assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .light)), named: "FEED_WITH_CONTENT_light")
         assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+        assert(
+            snapshot: sut.snapshot(
+                for: .iPhone15Pro(
+                    style: .light,
+                    contentSize: .extraExtraExtraLarge
+                )
+            ),
+            named: "FEED_WITH_CONTENT_light_extraExtraExtraLarge"
+        )
     }
 
     func test_feedWithFailedImageLoading() {
@@ -27,6 +36,15 @@ final class FeedSnapShotTests: XCTestCase {
 
         assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
         assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+        assert(
+            snapshot: sut.snapshot(
+                for: .iPhone15Pro(
+                    style: .light,
+                    contentSize: .extraExtraExtraLarge
+                )
+            ),
+            named: "FEED_WITH_FAILED_IMAGE_LOADING_light_extraExtraExtraLarge"
+        )
     }
 
     // MARK: - Helpers
