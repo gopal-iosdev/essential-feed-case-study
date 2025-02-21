06e0416af0d5a55bb1e4f97e721f26c7c29a6e2a
gopal-iosdev <gopal.iosdev@gmail.com>
2025-02-21 08:56:49 -0500

    Updated SnapshotConfiguration to use generic iPhone size

--- EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -16,8 +16,8 @@ final class ListSnapshotTests: XCTestCase {
 
         sut.display(emptyFeed())
 
-        assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .light)), named: "EMPTY_LIST_light")
-        assert(snapshot: sut.snapshot(for: .iPhone15Pro(style: .dark)), named: "EMPTY_LIST_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
     func test_listWithErrorMessage() {
@@ -26,15 +26,15 @@ final class ListSnapshotTests: XCTestCase {
         sut.display(.error(message: "This is a \nmulti-line\nerror message"))
 
         assert(
-            snapshot: sut.snapshot(for: .iPhone15Pro(style: .light)),
+            snapshot: sut.snapshot(for: .iPhone(style: .light)),
             named: "LIST_WITH_ERROR_MESSAGE_light"
         )
         assert(
-            snapshot: sut.snapshot(for: .iPhone15Pro(style: .dark)),
+            snapshot: sut.snapshot(for: .iPhone(style: .dark)),
             named: "LIST_WITH_ERROR_MESSAGE_dark"
         )
         assert(
-            snapshot: sut.snapshot(for: .iPhone15Pro(style: .light, contentSize: .extraExtraExtraLarge)),
+            snapshot: sut.snapshot(for: .iPhone(style: .light, contentSize: .extraExtraExtraLarge)),
             named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge"
         )
     }
