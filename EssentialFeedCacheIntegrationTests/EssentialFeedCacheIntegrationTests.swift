9dae98e7a3fb3a96ed38574dfc7d52a048171471
gopal-iosdev <gopal.iosdev@gmail.com>
2025-05-30 09:01:26 -0400

    Make LocalFeedLoader.load sync

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -152,19 +152,15 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         file: StaticString = #file,
         line: UInt = #line
     ) {
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, expectedFeed, file: file, line: line)
+        let result = Result { try sut.load() }
+        
+        switch result {
+        case let .success(imageFeed):
+            XCTAssertEqual(imageFeed, expectedFeed, file: file, line: line)
 
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
-            }
-
-            exp.fulfill()
+        case let .failure(error):
+            XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
         }
-        wait(for: [exp], timeout: 1.0)
     }
 
     private func save(
